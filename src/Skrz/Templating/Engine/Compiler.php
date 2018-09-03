49444e47e6ff3dddd433744be033b4c130f1eee6
Jiri Pokorny <pokorny@skrz.cz>
2018-09-03 10:38:24 +0200

    Do not allow nested block comments in compiled comments

--- src/Skrz/Templating/Engine/Compiler.php
+++ src/Skrz/Templating/Engine/Compiler.php
@@ -288,7 +288,9 @@ class Compiler extends AbstractASTWalker
 
 	protected function walkComment(CommentNode $comment)
 	{
-		return "/* {$comment->getComment()} */";
+		// Do not allow nested block comments
+		$comment = strtr($comment->getComment(), ["/*" => "/ *", "*/" => "* /"]);
+		return "/* {$comment} */";
 	}
 
 	protected function walkEcho(EchoNode $echo)
